b56716481dc32ae94a1b59a600f9cb636c8c16cc
Kirill-Lemesev <boyanya@gmail.com>
2023-10-28 22:25:24 +0500

    app bar added

--- task_04/lib/main.dart
+++ task_04/lib/main.dart
@@ -7,12 +7,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_rounded),
+              onPressed: () {  },
+              color: AppColors.button,
+            ),
+          title: Column(
+            children: [
+              Text(
+                  'Чек № 56',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.text
+                  )
+                ),
+              Text(
+                '24.02.23 в 12:23',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: AppColors.data
+                ),
+              )
+            ]
+          ),
+          centerTitle: true,
+          elevation: 0,
         ),
-      ),
+        body: Column(
+        ),
+        ),
     );
   }
 }
+
+
+
+
+
+abstract class AppColors {
+  static const background = Colors.white;
+  static const button = Color(0xff67cD00);
+  static const text = Color(0xff252849);
+  static const data = Color(0xff60607B);
+  static const strikeThrough = Color(0xffb5b5b5);
+  static const redText = Color(0xffff0000);
+}
+
