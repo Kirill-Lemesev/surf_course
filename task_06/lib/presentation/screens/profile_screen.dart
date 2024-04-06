a189d2275cd0ba9e54a001c31210acd8b1346554
Kirill-Lemesev <boyanya@gmail.com>
2024-04-06 19:41:19 +0500

    system state observer added

--- task_06/lib/presentation/screens/profile_screen.dart
+++ task_06/lib/presentation/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:surf_flutter_courses_template/assets/app_strings.dart';
 import 'package:surf_flutter_courses_template/assets/app_fonts.dart';
 import 'package:surf_flutter_courses_template/presentation/widgets/user_icon.dart';
@@ -13,7 +15,78 @@ class ProfileScreen extends StatefulWidget {
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfileScreenState extends State<ProfileScreen>
+    with WidgetsBindingObserver {
+  late String _appTheme;
+  late String _systemBrightness; //Brightness.light
+  late int _lightThemeScheme;
+  late int _darkThemeScheme;
+
+  @override
+  void initState() {
+    _loadTheme();
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _appTheme = prefs.getString('appTheme') ?? 'system';
+      _systemBrightness = prefs.getString('systemBrightness') ??
+          SchedulerBinding.instance.platformDispatcher.platformBrightness
+              .toString();
+      _lightThemeScheme = prefs.getInt('lightThemeScheme') ?? 1;
+      _darkThemeScheme = prefs.getInt('darkThemeScheme') ?? 1;
+    });
+  }
+
+  Future<void> _setAppTheme(String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.setString('appTheme', value);
+      _appTheme = value;
+    });
+  }
+
+  Future<void> _setSystemBrightness() async {
+    var currentPlatformBrightness = SchedulerBinding
+        .instance.platformDispatcher.platformBrightness
+        .toString();
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.setString('systemBrightness', currentPlatformBrightness);
+      _systemBrightness = currentPlatformBrightness;
+    });
+  }
+
+  Future<void> _setLightThemeScheme(int value) async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.setInt('lightThemeScheme', value);
+      _lightThemeScheme = value;
+    });
+  }
+
+  Future<void> _setDarkThemeScheme(int value) async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.setInt('lightThemeScheme', value);
+      _darkThemeScheme = value;
+    });
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    _setSystemBrightness();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -46,14 +119,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
               style: AppFonts.noStyle16,
             ),
             const MedalRow(),
-            const SizedBox(height: 10,),
-            FormItem(itemName: AppStrings.name, itemData: AppStrings.personName, showButton: false,),
-            FormItem(itemName: AppStrings.email, itemData: AppStrings.personEmail, showButton: false,),
-            FormItem(itemName: AppStrings.birthDate, itemData: AppStrings.personBirthDate, showButton: false,),
-            FormItem(itemName: AppStrings.team, itemData: AppStrings.personTeam, showButton: true,),
-            FormItem(itemName: AppStrings.position, itemData: AppStrings.personPosition, showButton: true,),
-            FormItem(itemName: AppStrings.formButtonText, itemData: AppStrings.systemTheme, showButton: true,),
-            const Expanded(child: SizedBox(height: 10,)),
+            const SizedBox(
+              height: 10,
+            ),
+            FormItem(
+              itemName: AppStrings.name,
+              itemData: AppStrings.personName,
+              showButton: false,
+            ),
+            FormItem(
+              itemName: AppStrings.email,
+              itemData: AppStrings.personEmail,
+              showButton: false,
+            ),
+            FormItem(
+              itemName: AppStrings.birthDate,
+              itemData: AppStrings.personBirthDate,
+              showButton: false,
+            ),
+            FormItem(
+              itemName: AppStrings.team,
+              itemData: AppStrings.personTeam,
+              showButton: true,
+            ),
+            FormItem(
+              itemName: AppStrings.position,
+              itemData: AppStrings.personPosition,
+              showButton: true,
+            ),
+            FormItem(
+              itemName: AppStrings.formButtonText,
+              itemData: AppStrings.systemTheme,
+              showButton: true,
+              //modalBottomSheet: ,
+            ),
+            const Expanded(
+                child: SizedBox(
+              height: 10,
+            )),
             const LogOutButton()
           ],
         ),
--- task_06/lib/presentation/widgets/form_item.dart
+++ task_06/lib/presentation/widgets/form_item.dart
@@ -5,14 +5,13 @@ class FormItem extends StatelessWidget {
   final String itemName;
   final String itemData;
   final bool showButton;
-  final Function? buttonFunc;
+  final Widget? modalBottomSheet;
 
-  const FormItem(
-      {super.key,
-      this.buttonFunc,
-      required this.itemName,
-      required this.itemData,
-      required this.showButton});
+  const FormItem({super.key,
+    required this.itemName,
+    required this.itemData,
+    required this.showButton,
+    this.modalBottomSheet});
 
   @override
   Widget build(BuildContext context) {
@@ -23,67 +22,72 @@ class FormItem extends StatelessWidget {
               elevation: 0,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16))),
-          onPressed: (buttonFunc == null) ? () {} : buttonFunc!(),
+          onPressed: (modalBottomSheet != null) ? () {
+            showModalBottomSheet(
+                context: context, builder: (BuildContext context) {
+              return modalBottomSheet!;
+            });
+          } : () {},
           child: (showButton)
               ? Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text(itemName, style: AppFonts.noStyle16),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Row(
-                              children: [
-                                Text(itemData, style: AppFonts.noStyle16),
-                              ],
-                            ),
-                          ]),
-                      const Expanded(
-                          child: SizedBox(
-                        height: 10,
-                      )),
-                      const Icon(
-                        Icons.arrow_forward_ios_sharp,
-                        size: 16,
-                      )
-                    ],
-                  ),
+                      Row(
+                        children: [
+                          Text(itemName, style: AppFonts.noStyle16),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Text(itemData, style: AppFonts.noStyle16),
+                        ],
+                      ),
+                    ]),
+                const Expanded(
+                    child: SizedBox(
+                      height: 10,
+                    )),
+                const Icon(
+                  Icons.arrow_forward_ios_sharp,
+                  size: 16,
                 )
+              ],
+            ),
+          )
               : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  itemName,
-                                  style: AppFonts.noStyle16,
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Row(
-                              children: [
-                                Text(itemData, style: AppFonts.noStyle16),
-                              ],
-                            ),
-                          ]),
-                    ],
-                  ),
-                )),
+                      Row(
+                        children: [
+                          Text(
+                            itemName,
+                            style: AppFonts.noStyle16,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Text(itemData, style: AppFonts.noStyle16),
+                        ],
+                      ),
+                    ]),
+              ],
+            ),
+          )),
     );
   }
 }
