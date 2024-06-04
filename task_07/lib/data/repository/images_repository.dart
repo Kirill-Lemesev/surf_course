5c38ed11156e423f86299c5cbcb5bf3e81a557d6
Kirill-Lemesev <boyanya@gmail.com>
2024-06-04 20:39:49 +0500

    assets and data

--- task_07/lib/data/repository/images_repository.dart
+++ task_07/lib/data/repository/images_repository.dart
@@ -1,6 +1,6 @@
 import 'package:surf_flutter_courses_template/data/mock_data.dart';
 import 'package:surf_flutter_courses_template/domain/entity/image.dart';
 
-Future <List<Image>> getData() {
-  return Future.delayed((const Duration(seconds: 5)),() => images);
+Future<List<Image>> getData() {
+  return Future.delayed((const Duration(seconds: 5)), () => images);
 }
