2072034a5aa12e124d2ca80f6b18753e230705bf
Kirill-Lemesev <boyanya@gmail.com>
2024-07-07 20:36:25 +0500

    text under radio button

--- task_09/lib/presentation/form.dart
+++ task_09/lib/presentation/form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:surf_flutter_courses_template/assets/app_colors.dart';
+import 'package:surf_flutter_courses_template/assets/app_text.dart';
 import 'package:surf_flutter_courses_template/assets/animal_types.dart';
 import 'radio_button.dart';
 
@@ -28,7 +29,7 @@ class _PetFormState extends State<PetForm> {
           padding: const EdgeInsets.symmetric(horizontal: 10.0),
           child: Column(
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               Row(
@@ -39,24 +40,28 @@ class _PetFormState extends State<PetForm> {
                     value: Animal.dog,
                     groupValue: _petFormState,
                     onChanged: _changePetFormState,
+                    buttonName: AppText.dog,
                   ),
                   PetRadioButton(
                     image: 'assets/cat.png',
                     value: Animal.cat,
                     groupValue: _petFormState,
                     onChanged: _changePetFormState,
+                    buttonName: AppText.cat,
                   ),
                   PetRadioButton(
                     image: 'assets/parrot.png',
                     value: Animal.parrot,
                     groupValue: _petFormState,
                     onChanged: _changePetFormState,
+                    buttonName: AppText.parrot,
                   ),
                   PetRadioButton(
                     image: 'assets/hamster.png',
                     value: Animal.hamster,
                     groupValue: _petFormState,
                     onChanged: _changePetFormState,
+                    buttonName: AppText.hamster,
                   ),
                 ],
               )
--- task_09/lib/presentation/radio_button.dart
+++ task_09/lib/presentation/radio_button.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:surf_flutter_courses_template/assets/app_colors.dart';
+import 'package:surf_flutter_courses_template/assets/app_fonts.dart';
 import 'package:surf_flutter_courses_template/assets/animal_types.dart';
 
 class PetRadioButton extends StatelessWidget {
   final String image;
+  final String buttonName;
   final Animal value;
   final Animal groupValue;
   final ValueChanged<Animal> onChanged;
@@ -13,23 +15,32 @@ class PetRadioButton extends StatelessWidget {
       required this.image,
       required this.value,
       required this.groupValue,
-      required this.onChanged});
+      required this.onChanged,
+      required this.buttonName});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => onChanged(value),
-      child: Container(
-        width: 72,
-        height: 72,
-        decoration: BoxDecoration(
-            color: (value == groupValue) ? AppColors.pink : AppColors.white,
-            borderRadius: BorderRadius.circular(10)),
-        child: ImageIcon(
-          size: 32,
-          AssetImage(image),
-          color: (value == groupValue) ? AppColors.white : AppColors.black,
-        ),
+      child: Column(
+        children: [
+          Container(
+            width: 72,
+            height: 72,
+            decoration: BoxDecoration(
+                color: (value == groupValue) ? AppColors.pink : AppColors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: ImageIcon(
+              size: 32,
+              AssetImage(image),
+              color: (value == groupValue) ? AppColors.white : AppColors.black,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(buttonName, style: AppFonts.blackTextSize12)
+        ],
       ),
     );
   }
